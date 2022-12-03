e2a6d95e934a39691dfa932e0e2723e9067f12b7
Mark Boom <git@mpboom.com>
2022-12-03 16:43:05 +0100

    Haskell day 2

---
+++ 02/Solution.hs
@@ -0,0 +1,23 @@
+import Data.Char
+
+updateResult (x, 2) = (x, x)
+updateResult (1, 1) = (1, 3)
+updateResult (2, 1) = (2, 1)
+updateResult (3, 1) = (3, 2)
+updateResult (1, 3) = (1, 2)
+updateResult (2, 3) = (2, 3)
+updateResult (3, 3) = (3, 1)
+
+score d = sum (map scorePerMatch d)
+    where scorePerMatch (a, b) | a == b = 3 + b
+                               | b == (a `mod` 3) + 1 = 6 + b
+                               | otherwise = b
+
+parse :: String -> [(Int, Int)]
+parse raw = map (\x -> (ord (x !! 0) - 64, ord (x !! 2) - 87)) (lines raw)
+
+solve = ((\x -> (part1 x, part2 x)) . parse) <$> readFile "input.txt"
+
+part1 = score
+
+part2 = score . (map updateResult)
