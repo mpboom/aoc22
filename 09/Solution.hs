e462817ce91a59c0e8625f22ddb51ab28c677a35
Mark Boom <git@mpboom.com>
2022-12-11 00:23:24 +0100

    Day 9 (part II)

--- 09/Solution.hs
+++ 09/Solution.hs
@@ -1,15 +1,16 @@
 import Text.Parsec
 import Data.List
 
-runInstruction ((t, h), acc) ins = ((fst x, snd x), (fst x):acc)
-    where x = newPos t h ins
+simulate knots instructions = snd $ foldl run (take knots (repeat (0, 0)), []) instructions
+    where run (state, acc) ins = (update state ins, (last $ update state ins):acc)
 
-newPos t@(tx, ty) (hx, hy) (x, y) | legalTail t newHead = ((tx, ty), newHead)
-                                  | otherwise = ((tx + stepX, ty + stepY), newHead)
-    where newHead = (hx + x, hy + y)
-          legalTail (tx, ty) (hx, hy) = (max (abs $ tx - hx) (abs $ ty - hy)) <= 1
-          stepX = step (fst newHead) tx
-          stepY = step (snd newHead) ty
+update (h@(hx, hy):t@(tx, ty):[]) i@(x, y) = [(hx + x, hy + y), (tx + (fst m), ty + (snd m))]
+    where m = tailMovement h t i
+update (h@(hx, hy):t@(tx, ty):s) i@(x, y) = (hx + x, hy + y):(update (t:s) (tailMovement h t i))
+
+tailMovement (hx, hy) t@(tx, ty) (x, y) | legalTail t (hx + x, hy + y) = (0, 0)
+                                        | otherwise = (step (hx + x) tx, step (hy + y) ty)
+    where legalTail (tx, ty) (hx, hy) = (max (abs $ tx - hx) (abs $ ty - hy)) <= 1
           step a b = if a > b then (min 1 (a - b)) else (max (-1) (a - b))
 
 parser = concat <$> (many1 line)
@@ -17,5 +18,6 @@ parser = concat <$> (many1 line)
           steps d n = take n $ ((repeat . match) d)
           match d = case d of 'R' -> (1, 0) ; 'U' -> (0, 1) ; 'L' -> (-1, 0) ; 'D' -> (0, -1)
 
-solve = (\(Right x) -> (part1 x)) <$> ((parse parser "") <$> readFile "input.txt")
-    where part1 x = (length . nub . snd) $ foldl runInstruction (((0, 0), (0, 0)), []) x
+solve = (\(Right x) -> (part1 x, part2 x)) <$> ((parse parser "") <$> readFile "input.txt")
+    where part1 = length . nub . (simulate 2)
+          part2 = length . nub . (simulate 10)
